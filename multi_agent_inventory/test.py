c04df687b21ce969190af25341f3c83ceab8652d
Golthi Venkatacharyulu <119730058+venkatesh1545@users.noreply.github.com>
2025-04-25 20:43:41 +0530

    Delete multi_agent_inventory directory

--- multi_agent_inventory/test.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-import pandas as pd
-db_path='database/inventory.db'
-with sqlite3.connect(db_path) as conn:
-    df = pd.read_sql_query("SELECT * FROM inventory_monitoring LIMIT 1", conn)
-    print(df.columns.tolist())
